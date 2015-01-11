2c393bc791fbfbeb58ac6c553a88f270825d654c
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-11 13:12:18 +0000

    initialise project as blank compojure template

---
+++ src/osra_api_clj/handler.clj
@@ -0,0 +1,23 @@
+(ns osra-api-clj.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [osra-api-clj.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "osra-api-clj is starting"))
+
+(defn destroy []
+  (println "osra-api-clj is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
