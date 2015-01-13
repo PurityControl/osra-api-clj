8be8a04c7263537921080dd2750862de7b28f804
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-13 08:17:54 +0000

    make root 200 response a json response

--- src/osra_api_clj/routes/home.clj
+++ src/osra_api_clj/routes/home.clj
@@ -1,11 +1,13 @@
 (ns osra-api-clj.routes.home
   (:require [compojure.core :refer :all]
+            [cheshire.core :refer [generate-string]]
             [liberator.core
               :refer [defresource resource request-method-in]]))
 
 (defresource home
-  :handle-ok "Success"
-  :etag "fixed-etag" :available-media-types ["text/plain"])
+  :handle-ok (fn [_] (generate-string "Success"))
+  ;;:handle-ok (generate-string "Success")
+  :etag "fixed-etag" :available-media-types ["text/json"])
 
 (defroutes home-routes
   (GET "/" request home))
